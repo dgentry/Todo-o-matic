3c46058ac60744643e5451482f981cd3a13e9c5d
Dennis Gentry <dennis.gentry@gmail.com>
2012-06-10 14:02:26 -0700

    Consolidate on taskmerge.py

--- taskmerge.py
+++ taskmerge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-#  Merge (dated) tasks from multiple todo.txt style files
+#  Merge multiple todo.txt style files
 #
 
 key =\
@@ -21,14 +21,32 @@ import string
 
 from datetime import datetime, timedelta
 
-#from logging import basicConfig, info, debug, DEBUG, warn, WARNING
-from logging import basicConfig, debug, DEBUG
-basicConfig(level=DEBUG, format='%(message)s')
+from logging import basicConfig, debug, WARNING
+#from logging import basicConfig, debug, WARNING
+basicConfig(level=WARNING, format='%(message)s')
 
-from g_utils import colorz, YELLOW, WHITE  # , RED, GREEN, BRIGHT
+from g_utils import colorz, YELLOW, WHITE, RED, BRIGHT, GREEN, GREY50
 
 
 files_to_merge = ["~/txt/todo/today.txt", "~/txt/todo/today-glance3.txt"]
+
+
+class Task(object):
+
+    def __init__(self):
+        self.datetime = None
+        self.text = ''
+        self.lineNumber = 0
+
+
+def uniq(aList):
+    result = []
+    for item in aList:
+        if not item in result:
+            result.append(item)
+        else:
+            debug("Uniq'ed out %s" % str(item))
+    return result
 
 
 def datetimeFromString(s):
@@ -86,90 +104,116 @@ def reasonableDateString(dt):
     return rs
 
 
-def merge():
-    chunks = dict()
-    for filename in files_to_merge:
-        fn = os.path.expanduser(filename)
+def parse_one_todo_file(filename):
+    """Given a filename, returns a corresponding list of chunks"""
+    usable_filename = os.path.expanduser(filename)
+    chunks = []
 
-        chunks[filename] = []
+    text = open(usable_filename, "r").readlines()
 
-        text = open(fn, "r").readlines()
-        # Break file into chunks that begin here and end with a "delimiter line"
+    # Break file into chunks that begin here and end with a "delimiter
+    # line."  Delimiter lines begin with "-----".  Old style ones might
+    # have a date on the next line, new ones with the date on the same
+    # line
 
-        # Delimiter lines begin with "-----".  Old style ones might have a
-        # date on the next line, new ones with the date on the same line
+    paragraph = []  # Paragraph is a list of date and lines tuples (today)
+    max_date_str = "14 June 2063 23:59:59"
+    debug("max_date_str = %s", max_date_str)
 
-        timechunk = []  # Timechunk is a list of date and lines tuples (today)
-        max_date_str = "14 June 2063 23:59:59"
-        debug("max_date_str = %s", max_date_str)
+    # Start at newest (im)possible date
+    last_date_found = datetimeFromString(max_date_str)
+    debug("last date = %s", str(last_date_found))
 
-        # Start at newest (im)possible date
-        last_date_found = datetimeFromString(max_date_str)
-        debug("last date = %s", str(last_date_found))
+    #
+    # Loop through the lines in the file
+    #
+    for i in range(0, len(text) - 1):
+        #debug('%d: %s' % (i, text[i]))
 
-        #
-        # Loop through the lines in the file
-        #
-        for i in range(0, len(text) - 1):
-            #debug('%d: %s' % (i, text[i]))
+        # '-----' starts a new chunk
+        if text[i].startswith('-----'):
+            # Finish up the previous chunk
+            if len(paragraph) > 0:
+                chunks.append((last_date_found, paragraph))
 
-            # '-----' starts a new chunk
-            if text[i].startswith('-----'):
-                # Finish up the previous chunk
-                if len(timechunk) > 0:
-                    chunks[filename].append((last_date_found, timechunk))
+            # Start a new chunk
+            paragraph = []
+            lineNumber = 0
+            s = text[i].strip('- \t\n\r')
+            trial_date = datetimeFromString(s)
+            if trial_date:
+                last_date_found = trial_date
+            #debug("trial_date one = %s" % str(trial_date))
 
-                # Start a new chunk
-                timechunk = []
+        else:  # Text doesn't start with '-----'
+            if trial_date == None:  # And we haven't found a date yet
                 s = text[i].strip('- \t\n\r')
                 trial_date = datetimeFromString(s)
                 if trial_date:
                     last_date_found = trial_date
-                debug("trial_date one = %s" % str(trial_date))
+                    #debug("Second line date = %s" % str(trial_date))
+                else:
+                    last_date_found = last_date_found - timedelta(minutes=1)
+                    debug("Using prior last_date_found - 1 min = %s" %\
+                          str(last_date_found))
+            else:  # trial_date already validly set, normal body line
+                paragraph.append((trial_date, lineNumber, text[i].rstrip()))
+                lineNumber -= 1
 
-            else:  # Text doesn't start with '-----'
-                if trial_date == None:  # And we haven't found a date yet
-                    s = text[i].strip('- \t\n\r')
-                    trial_date = datetimeFromString(s)
-                    if trial_date:
-                        last_date_found = trial_date
-                        debug("Second line date = %s" % str(trial_date))
-                    else:
-                        last_date_found = last_date_found - timedelta(minutes=1)
-                        debug("Using prior last_date_found - 1 min = %s" %\
-                              str(last_date_found))
-                else:  # trial_date already validly set
-                    timechunk.append((trial_date, text[i].rstrip() + '\n'))
+        if string.count(text[i], '<snip>'):
+            debug("<snip> Done.")
+            break
 
-            if string.count(text[i], '<snip>'):
-                debug("<snip> Done.")
+    for paragraph in chunks:
+        debug(colorz(reasonableDateString(paragraph[0]), YELLOW))
+        debug(colorz(''.join(repr(paragraph[1])), RED))
+    debug("\n\n")
 
-    # If debugging, print out what we have without merging, uniquing or sorting
-    for filename in chunks:
-        for timechunk in chunks[filename]:
-            debug(colorz(reasonableDateString(timechunk[0][0]), YELLOW))
-            debug(colorz(''.join(timechunk[1]), WHITE))
-        debug("\n\n")
+    return chunks
 
-    # make one list
-    theList = chunks[files_to_merge[0]] + chunks[files_to_merge[1]]
+
+def string_tasktuple(task, withDate=False, withLineNumber=False):
+    debug(colorz(task, BRIGHT))
+
+    firstPart = ''
+    if withDate:
+        firstPart = colorz(reasonableDateString(task[0]), GREEN) + ' '
+    if withLineNumber:
+        firstPart += colorz(str(task[1]), GREY50) + ' '
+
+    return firstPart + colorz(''.join(task[2]), WHITE)
+
+
+def merge(files_to_merge):
+    "Iterate over files_to_merge, parsing into chunks, merging, sorting, uniquing"
+    theList = []
+    for filename in files_to_merge:
+        theList += parse_one_todo_file(filename)
+
+    # sort the list
     sList = sorted(theList, reverse=True)
 
     # Uniq it
-    uList = []
-    for x in sList:
-        if not x in uList:
-            uList.append(x)
+    uList = uniq(sList)
+
+    # Iterate over the chunks and accumulate a new, flat, task-level list
+    flatList = []
+    #taskDict = dict()
+    for paragraph in uList:
+        for task in paragraph[1]:
+            debug(string_tasktuple(task))
+            flatList.append(task)
+
+    sortedTasks = sorted(flatList, reverse=True)
+
+    uniqueTasks = uniq(sortedTasks)
+
+    # Now print them out (print a date on only the tasks from line 0)
+    for task in uniqueTasks:
+        if task[1] == 0:
+            print string_tasktuple(task, withDate=True)
         else:
-            print "Uniq'ed out %s", x
-
-    print len(uList)
-
-    # Output the chunks
-    for timechunk in uList:
-        print colorz(reasonableDateString(timechunk[0]), YELLOW)
-        print colorz(''.join(timechunk[1]), WHITE)
-
+            print string_tasktuple(task)
 
 if __name__ == "__main__":
-    merge()
+    merge(files_to_merge)
